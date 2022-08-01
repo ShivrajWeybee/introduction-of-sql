d9b00d76cd50a5bfc3c587d887bf9fbacbadde0f
ShivrajWeybee <107666663+ShivrajWeybee@users.noreply.github.com>
2022-08-01 12:44:46 +0530

    Add files via upload

--- intoduction_task.sql
+++ intoduction_task.sql
@@ -3,22 +3,24 @@ select * from Customer;
 select * from Product;
 select * from Shipping;
 select * from Orders;
-select * from Order_items;
+select * from OrderItems;
 
 -------------------- 1 --------------------
-select * from Customer where last_name like 'MY%'
+/*select * from Customer where last_name like 'MY%'
 union
-select * from Customer where last_name like '%SE%';
+select * from Customer where last_name like '%SE%';*/
+
+select * from Customer where last_name like 'MY%' OR last_name like '%SE%';
 
 -------------------- 2 --------------------
 select * from Customer where last_name like '%B[ru]%'
 
 -------------------- 3 --------------------
-SELECT * FROM Order_items
-WHERE unit_price > (SELECT  AVG(unit_price) FROM Order_items );
+SELECT * FROM OrderItems
+WHERE unit_price > (SELECT  AVG(unit_price) FROM OrderItems );
 
 -------------------- 4 --------------------
-select * from Order_items 
+select * from OrderItems 
 order by quantity*unit_price desc
 
 -------------------- 5 --------------------
@@ -26,5 +28,5 @@ select ot.order_item_id, o.order_id, c.first_name, p.product_name, s.shipper_nam
 from (Customer c
 inner join Orders o on c.customer_id=o.customer_id)
 inner join Shipping s on s.shipper_id=o.shipper_id
-inner join Order_items ot on ot.order_id=o.order_id
+inner join OrderItems ot on ot.order_id=o.order_id
 inner join Product p on p.product_id=ot.product_id;
